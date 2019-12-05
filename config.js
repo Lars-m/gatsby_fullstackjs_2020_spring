0745995ccca316a4a653064e6f49957a55280b03
lars-m <lam@cphbusiness.dk>
2019-12-05 10:45:12 +0100

    FS spring 2020

---
+++ config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title1: `FullStack JavaScript`,
+  title2: `FullStack JavaScript - Spring 2020`,
+  titleShort: `FullStack JS`,
+  idLocalStorage:"fullstackspring2020",
+  classes: [{ value: 'a', label: 'A-class',backgroundColor:"#295683" },
+  //   { value: 'b', label: 'B-class',backgroundColor:"#700909" },
+  //  { value: 'c', label: 'Bornholm',backgroundColor: "#cc6600" }
+  ],
+  showWeekInfoForEachDayInWeek : false,
+  timeEdit: "https://cloud.timeedit.net/cphbusiness/web/student",
+  topMenu: [
+    {
+      title:"StudyPoints",
+      URL: "https://studypoints.info"
+    },
+    {
+      title:"Schedule",
+      route: "/full-schedule"
+    },
+    {
+      title:"Goals",
+      route: "/all-goals"
+    },
+    {
+      title:"Repos",
+      route: "/github-links"
+    },
+    // {
+    //   title:"Exercises",
+    //   route: "/all-exercises"
+    // },
+    // {
+    //   title:"Read",
+    //   route: "/all-readings"
+    // },
+    {
+      title:"Links",
+      //These are the only legal values, Update this if making changes
+      // "guidelines","readings","exercises","admin","examprep","studypointex","ca","slides",
+      include: ["guidelines","readings","exercises","admin","examprep","studypointex","ca","slides"],
+      route: "/links"
+    },
+    {
+      title:"About",
+      route: "/"
+    },
+  ]
+}
---
+++ src/pages/Flow-1/week3/SP3.md
@@ -0,0 +1,21 @@
+---
+title: "ADD TITLE"
+date : '06-09-2019'
+decorator: "SP3 (#date#)"
+isSP: true
+---
+<!-- REMOVE ME: Setting isSP ensures this pages gets added to the list of Studypoint exercises -->
+
+## Part-1 Complete all exercises (according to your colour level) from this week
+
+<!-- REMOVE ME: The tag below will insert all day-exercises given for this week -->
+<!-- REMOVE ME: PeriodFolder and weekFolder MUST match the real folder names -->
+
+<!--PeriodExercises Flow-1/week3 PeriodExercises--> 
+
+## Part-2 
+### Add a JavaScript-driven frontend to your REST-API
+
+Continue with part-2 of the exercise "*REST, JPA, Test and CI with our start code*" where you implemented a small movie-application.
+Add a simple JavaScript-driven page to the project that fetches and render data from one, or more, of your REST-endpoints.
+Make sure to deploy via Travis and not manually.
